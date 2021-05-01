bb89c04e3040e3a2a9155d2c77af5800b6979811
Felipekcomt <felipe_kcomt@hotmail.com>
2021-05-01 18:08:11 -0500

    feat: add explore feature to show new places

--- src/app/pages/place-item/place-item.component.ts
+++ src/app/pages/place-item/place-item.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import {CountryService} from "../../services/country.service";
+import {CityService} from "../../services/city.service";
+import {PlaceService} from "../../services/place.service";
+import {Country} from "../../model/Country";
+import {City} from "../../model/City";
+import {Place} from "../../model/Place";
+import {FavoriteService} from "../../services/favorite.service";
+import {Favorite} from "../../model/Favorite";
+import {any} from "codelyzer/util/function";
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-place-item',
   templateUrl: './place-item.component.html',
@@ -7,9 +16,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceItemComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private countryService: CountryService, private cityService: CityService, private placeService: PlaceService, private favoriteService: FavoriteService, private router: Router) { }
+  countries: Country[] = []
+  cities: City[] = []
+  places: Place[] = []
+  favorite: Favorite[] = []
   ngOnInit(): void {
+    this.countryService.getAllCountries().subscribe((response: any) => {
+      this.countries = response
+      console.log(this.countries)
+      this.shuffleItems(this.countries)
+    })
+    this.cityService.getAllCities().subscribe((response: any) => {
+      this.cities = response
+      console.log(this.cities)
+      this.shuffleItems(this.cities)
+      console.log(this.cities)
+    })
+    this.placeService.getAllPlaces().subscribe((response:any) => {
+      this.places = response
+      this.shuffleItems(this.places)
+    })
+    this.favoriteService.getFavoriteByUserId('1').subscribe((response: any) => {
+      this.favorite = response
+    })
+  }
+  shuffleItems(item: any[]) {
+     let counter = item.length;
+    while (counter > 0) {
+      let index = Math.floor(Math.random() * counter);
+      counter--;
+      [item[counter], item[index]] = [item[index], item[counter]];
+    }
+  }
+  redirectTo(uri:string,param:string){
+    this.router.navigateByUrl('/', {skipLocationChange: true}).then(()=>
+      this.router.navigate([uri,param]));
   }
 
 }
---
+++ src/app/services/favorite.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Favorite} from "../model/Favorite";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavoriteService {
+
+  URI: string = `${environment.HOST_URL}/users`
+  constructor(private htpp: HttpClient) { }
+
+  getFavoriteByUserId(userId) {
+   return this.htpp.get<Favorite[]>(`${this.URI}/${userId}/locatables`)
+  }
+
+}

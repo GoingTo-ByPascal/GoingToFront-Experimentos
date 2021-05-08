55f05f220a3ab6920ae76cab2bf99e491c9218c2
Lino <u201819681@upc.edu.pe>
2021-05-08 00:32:01 -0500

    fix step and logo pointer

---
+++ testing/features/search/search.steps.js
@@ -0,0 +1,22 @@
+import { Given, Then, When } from "cypress-cucumber-preprocessor/steps";
+
+Given("El usuario escoge un lugar", () => {
+  cy.visit("http://localhost:4200");
+  cy.get("#search").click();
+  cy.wait(5000);
+});
+When("Hace click en el lugar escogido", () => {
+  cy.get("#Peru").click();
+  cy.get("#viewMoreTips").click();
+});
+Then("El sistema despliega los tips del lugar escogido", () => {
+  cy.get("h1").contains("Tips");
+});
+
+Then("El sistema despliega las promociones del lugar escogido", () => {
+  cy.get("h1").contains("Promos");
+});
+
+Then("El sistema despliega las reviews del lugar escogido", () => {
+  cy.get("h1").contains("Reviews");
+});

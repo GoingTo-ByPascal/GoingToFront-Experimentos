2392993963893e56ad0616bcd928ee80540cc5f4
Lino <u201819681@upc.edu.pe>
2021-06-21 22:53:14 -0500

    added register featured

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,8 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RegisterService {
-  constructor() {}
+  URI1: string = `${environment.HOST_URL}/UserProfiles`;
+  URI2: string = `${environment.HOST_URL}/Users`;
+  constructor(private http: HttpClient) {}
+  firstRegister(body) {
+    return this.http.post(this.URI2, body);
+  }
+  secondRegister(body) {
+    return this.http.post(this.URI1, body);
+  }
 }
